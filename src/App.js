5cd536391b91a850d98de773af2fa9e0e75ce783
David Vlasic <dvlasic000@gmail.com>
2023-10-06 09:32:35 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './componenets/content';
+import Footer from './componenets/footer';
+import Header from './componenets/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Container>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path='/' element={<Content></Content>}></Route>
+          <Route path='/read' element={<Header></Header>}></Route>
+          <Route path='/create' element={<Footer></Footer>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ src/componenets/footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <div className="App">
+            <h1>My footer in another component</h1>
+        </div>
+    );
+}
+export default Footer;
